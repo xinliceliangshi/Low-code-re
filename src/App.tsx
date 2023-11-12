a87abf6fdb3c437f533712ae5fd5b177b5618b88
wangying <wangying@stargraph.cn>
2023-11-12 12:04:58 +0800

    feat: 添加列表

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
-import logo from './logo.svg'
+// import logo from './logo.svg'
 import './App.css'
 
 function App() {
+  const questionList = [
+    { id: 'q1', title: '问卷1', isPublished: true },
+    { id: 'q2', title: '问卷2', isPublished: false },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>问卷列表页</h1>
+      <div>
+        {questionList.map(question => {
+          const { id, title, isPublished } = question
+          return (
+            <div key={id}>
+              <span>{title}</span>
+              <span>{isPublished ? '已发布' : '未发布'}</span>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
